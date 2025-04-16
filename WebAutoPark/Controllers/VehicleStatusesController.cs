ec3b685d1263eb16f82ab48dfb7a1e8de7c2f607
Radon Nazar <radon_ak23@nuwm.edu.ua>
2025-04-16 17:52:05 +0300

    Begin working app begin CRUD

---
+++ WebAutoPark/Controllers/VehicleStatusesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAutoPark.Data;
+using WebAutoPark.Data.Entities;
+using WebAutoPark.Models;
+
+namespace WebAutoPark.Controllers
+{
+    public class VehicleStatusesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppAutoParkContext _context;
+
+        public VehicleStatusesController(AppAutoParkContext context, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _context.VehicleStatuses.ToList(); 
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> Create(VehicleStatusEntity model)
+        {
+            await _context.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _context.VehicleStatuses.SingleOrDefaultAsync(x => x.Id == id);
+            if (item != null)
+            {
+                _context.VehicleStatuses.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
